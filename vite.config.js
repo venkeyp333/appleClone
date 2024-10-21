84c96db8e0423597aeb1387376793b123d9764b4
venkateshp <venkateshp.cdac.in>
2024-10-21 18:51:12 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/appleClone/',  // Set this to your repository name
   plugins: [react()],
 })
